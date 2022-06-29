7d02d9bf449ef3930a03a525f627c90258b3f554
AkanH2021 <ahodja@web.de>
2022-06-29 14:36:58 +0200

    Additional consultatioin Java with E.Grajdansky - Classes, Interface, inheritance example

---
+++ Gr26_Lectures/Java/Facultative29June/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+    Dog zhuchka = new Dog("Zhucka", 2);
+        System.out.println(zhuchka);
+
+        zhuchka.bark();
+        zhuchka.run();
+
+
+
+    }
+}
---
+++ Gr26_Lectures/Java/Facultative29June/src/transformer/Main.java
@@ -0,0 +1,38 @@
+package transformer;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    String input = "aaa BBBb cDvCn ddddddddd";
+
+    TransformToUpper toUpperCase = new TransformToUpper();
+    System.out.println(transformer(input, toUpperCase));
+
+    TransformToLower toLowerCase = new TransformToLower();
+    System.out.println(transformer(input, toLowerCase));
+
+    TransformToStars toStars = new TransformToStars();
+    System.out.println(transformer(input, toStars));
+
+    }
+    //Дана строка, состоящая из нескольких слов разделенных пробелом.
+    // Написать метод, позволяющий изменять слова в строке так:
+    // при длине слова = 3, изменить все буквы слова на заглавные
+    // при длине слова = 4, изменить все буквы слова на строчные
+    // при длине слова = 5, изменить все буквы слова на звездочки
+    // "aaa bbbb BBBb ccc dddddd dRgHd"
+
+    public static String transformer(String string1, WordsTransformer trans1) {
+
+        String[] words = string1.split(" "); // split unterteilt den String anhand der darauf folgenden Angabe
+
+        for (int i = 0; i < words.length; i++) {
+            if (trans1.check(words[i])) {
+                words[i] = trans1.transform(words[i]);
+            }
+        }
+        return String.join(" ", words);     // join macht hier split wieder rückgängig
+    }
+
+}
