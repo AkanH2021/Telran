70fc40488dc5bf35aacd2af43e49c7e618d667b8
AkanH <ahodja@web.de>
2022-08-29 00:29:59 +0200

    HW Algorithms - 17.08.2022

--- Gr26_HW_Algo/Algorithms - 17.08.2022/src/LiftChooser.java
+++ Gr26_HW_Algo/Algorithms - 17.08.2022/src/LiftChooser.java
@@ -6,7 +6,7 @@ public class LiftChooser {
         int lowestFloor;
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter lowest floor of the building");
-        lowestFloor = sc.nextInt();
+        lowestFloor = sc.nextInt();     //defining lowest floor of the building
         return lowestFloor;
     }
 
@@ -14,7 +14,7 @@ public class LiftChooser {
         int highestFloor;
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter highest floor of the building");
-        highestFloor = sc.nextInt();
+        highestFloor = sc.nextInt();    //defining highest floor of the building
         return highestFloor;
     }
 
@@ -22,7 +22,7 @@ public class LiftChooser {
         int currentCall;
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter your current floor");
-        currentCall = sc.nextInt();
+        currentCall = sc.nextInt();     //defining current position of the caller
         return currentCall;
     }
 
@@ -31,13 +31,13 @@ public class LiftChooser {
         int high = setHighestFloor();
         int call = setCurrentCall();
 
-        int border1 = getRandomNumber1(low, high);
-        int border2 = getRandomNumber1(low, high);
+        int border1 = getRandomNumber(low, high);  //defining random position of the lift1
+        int border2 = getRandomNumber(low, high);  //defining random position of the lift2
 
         System.out.println("You are currently on the :" + call + " floor");
 
-        int length1 = Math.abs(call - border1);
-        int length2 = Math.abs(call - border2);
+        int length1 = Math.abs(call - border1);     //defining travel length of lift1
+        int length2 = Math.abs(call - border2);     //defining travel length of lift2
 
         System.out.println("Lift A is on: " + border1 + " floor, need to travel " + length1 + " floors");
         System.out.println("Lift B is on: " + border2 + " floor, need to travel " + length2 + " floors");
@@ -52,7 +52,7 @@ public class LiftChooser {
         }
     }
 
-    public int getRandomNumber1(int min, int max) {
+    public int getRandomNumber(int min, int max) {
         return (int) ((Math.random() * (max - min) + 1) + min);
     }
 
