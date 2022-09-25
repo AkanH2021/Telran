d74aabb0d5f3a4e53e1f6d8ea925a80402d2a896
Akan H <ahodja@web.de>
2022-09-25 23:27:35 +0200

    Java-20.09.2022 HW

--- Gr26_HW_Java/Java-22.09.2022/src/additionalTask/Main.java
+++ Gr26_HW_Java/Java-22.09.2022/src/additionalTask/Main.java
@@ -9,13 +9,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
-     /*  - "src/main/java/WarAndPeace.txt это ссылка на какой либо текст.
-      Необходимо пройти стримом по тексту написать метод который считает количество уникальных слов.
-      long count = words.stream().distinct().count();
-      И количество повторений long countWithRepeat =тут тоже будет стрим   *//*   тут обработать String contents  *//*
-        System.out.println("Слов всего " + countWithRepeat);
-        System.out.println("Уникальных слов  " + count);
-        P.S. Используйте так же регулярные выражения.
+     /*  -Задание из task 4 -> дополнительное условие:
   ********Нужно вывести каждое слово и количество его повторений одним стримом********
         */
     public static void main(String[] args) throws IOException {
