71d4f0844a892e804006778ac3a7ee818fe19714
Akan H <ahodja@web.de>
2023-01-12 10:32:08 +0100

    HW from 12.01.2023

--- Gr26_HW_Java/Collections/Map/HW12-01-2023/src/Main.java
+++ Gr26_HW_Java/Collections/Map/HW12-01-2023/src/Main.java
@@ -6,24 +6,18 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        //Из заданного массива слов заполнить ArrayList( 1000 ) случайными словами.
         List<String> wordsLIst = fulfillListRandomly();
-
         System.out.println(wordsLIst);
-
-        //Подсчитать сколько каждое слово встречается раз в ArrayList.
         System.out.println(everyWordFrequencyCount(wordsLIst));
-
-        //Вывести слово, которое повторяется чаще всего.
         System.out.println(maxRepeatedWord(everyWordFrequencyCount(wordsLIst)));
-
-        //Если таких слов несколько - вывести наименьшее из них.
-        System.out.println(shortestRepeatedWord(everyWordFrequencyCount(wordsLIst)));
+        System.out.println(maxRepeatedWords(everyWordFrequencyCount(wordsLIst)));
     }
 
+    //Из заданного массива слов заполнить ArrayList( 1000 ) случайными словами.
     public static List<String> fulfillListRandomly() {
         String sample;
-        List<String> result = new ArrayList<>(1000);
+        int size = 1000;
+        List<String> result = new ArrayList<>(size);
         Random r = new Random();
         try {
             sample = Files.readString(Paths.get("src/sample.txt"));
@@ -32,12 +26,13 @@ public class Main {
         }
         String[] words = (sample.split("[\\s,]+"));
 
-        for (int i = 0; i < words.length; i++) {
+        for (int i = 0; i < size; i++) {
             result.add(words[r.nextInt(0, words.length)]);
         }
         return result;
     }
 
+    //Подсчитать сколько каждое слово встречается раз в ArrayList
     public static Map<String, Integer> everyWordFrequencyCount(List<String> stringList) {
         if (stringList == null || stringList.isEmpty()) {
             return new HashMap<>();
@@ -46,6 +41,7 @@ public class Main {
                 .collect(Collectors.toMap(k -> k, v -> 1, Integer::sum));
     }
 
+    //Вывести слово, которое повторяется чаще всего.
     public static Map.Entry<String, Integer> maxRepeatedWord(Map<String, Integer> map) {
         Optional<Map.Entry<String, Integer>> maxEntry = map.entrySet()
                 .stream()
@@ -54,7 +50,7 @@ public class Main {
     }
 
     //Если таких слов несколько - вывести наименьшее из них.
-    public static List<String> shortestRepeatedWord(Map<String, Integer> map) {
+    public static List<String> maxRepeatedWords(Map<String, Integer> map) {
         Integer max = Objects.requireNonNull(map.entrySet()
                         .stream()
                         .min((entry1, entry2) -> entry1.getValue() < entry2.getValue() ? 1 : -1)
